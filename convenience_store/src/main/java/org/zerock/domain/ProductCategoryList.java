f260f028fb96c836faafa2097673defbc7c31b55
kjlee <ranian129@gmail.com>
2016-07-02 17:14:32 +0900

    Add missing domain class.

---
+++ convenience_store/src/main/java/org/zerock/domain/ProductCategoryList.java
@@ -0,0 +1,21 @@
+package org.zerock.domain;
+
+public class ProductCategoryList {
+
+	private Product product;
+	private CollaboReview collaboReview;
+	
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	public CollaboReview getCollaboReview() {
+		return collaboReview;
+	}
+	public void setCollaboReview(CollaboReview collaboReview) {
+		this.collaboReview = collaboReview;
+	}
+	
+}
