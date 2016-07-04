e7f6eed7ce9ba64baf4148a5109820a8de74e108
kjlee <ranian129@gmail.com>
2016-07-04 14:19:15 +0900

    add missing domain class.

---
+++ convenience_store/src/main/java/org/zerock/domain/User.java
@@ -0,0 +1,57 @@
+package org.zerock.domain;
+
+import java.util.Date;
+
+public class User {
+
+	private int user_id;
+	private String email;
+	private String password;
+	private String nickname;
+	private Date regdate;
+	private Date updatedate;
+	private String photo;
+	
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	public Date getUpdatedate() {
+		return updatedate;
+	}
+	public void setUpdatedate(Date updatedate) {
+		this.updatedate = updatedate;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+}
